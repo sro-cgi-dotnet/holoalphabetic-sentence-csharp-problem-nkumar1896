2d344078c66e7bcda94748ec0189189572252e55
narendar kumar <nkumar010896@gmail.com>
2018-09-17 10:17:41 +0530

    last edit

--- HoloalphabeticSentence/HoloalphabeticSentence.cs
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs
@@ -8,12 +8,14 @@ namespace HoloalphabeticSentence
         // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
         public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
         {
+            //it will caonver alphabets string into lower order if it is not
             input=input.ToLower();
             int s=input.Length;
             int k=0;
         for(int i=0;i<26;i++)
          {
           k=0;
+          //for every alphabet from 1 to 26 it will check if it present in the given string or not
          for(int j=0;j<s;j++)
          {
              if(alphabets[i]==input[j])
@@ -27,6 +29,7 @@ namespace HoloalphabeticSentence
              break;
          }
          }
+         //if alphabet not present in the given string, it will return false else will return true
          if(k==0)
          {
           return false;
